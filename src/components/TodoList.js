53b4252ab8ed90f6858a43de3bd1bb290680a069
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-01-12 12:44:14 -0500

    updated was on wrong branch

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+//Function Component
+
+import React from "react"
+import Todo from "./Todo";
+
+const TodoList = props => {
+console.log(props)
+  return (
+    <div className="todo-list">
+      {props.tasks &&  props.tasks.map(item => (
+
+      <Todo toggleTask={props.toggleTask}
+        key={item.id} task={item}
+      />
+       ))} 
+      <button  className="clear-btn" onClick={props.clearCompleted}>
+      Clear Completed
+      </button>
+    </div>
+  )
+}
+
+export default TodoList;
